1939db5198bdb3c608025b4c00a24482efa1e779
Alice Darner <alice.darner@svt.se>
2018-04-19 10:37:49 +0200

    ctx

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 var canvas = document.getElementById("myCanvas");
+canvas.style.width = (canvas.width = 500) + "px";
+canvas.style.height = (canvas.height = 500) + "px";
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#FF0000";
-ctx.fillRect(0,0,150,75);
+ctx.fillRect(0,0,canvas.width,canvas.height);
+ctx.fillStyle = "#FFFFFF";
+ctx.arc(canvas.width/2, canvas.height/2, canvas.height/2, 0, 2 * Math.PI, false);
+ctx.fill();
